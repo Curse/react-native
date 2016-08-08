463ce3f05d65285eba881d996f2fa8610000cb96
Ben Gordon <bgordon@curse.com>
2016-08-08 08:43:55 -0700

    Try/Catch around back task

--- ReactAndroid/src/main/java/com/facebook/react/modules/core/DeviceEventManagerModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/core/DeviceEventManagerModule.java
@@ -42,8 +42,11 @@ public class DeviceEventManagerModule extends ReactContextBaseJavaModule {
       @Override
       public void run() {
         UiThreadUtil.assertOnUiThread();
-        //backBtnHandler.invokeDefaultOnBackPressed();
-        getCurrentActivity().moveTaskToBack(true);
+        try {
+          getCurrentActivity().moveTaskToBack(true);
+        } catch (Exception ex) {
+          backBtnHandler.invokeDefaultOnBackPressed();
+        }
       }
     };
   }
